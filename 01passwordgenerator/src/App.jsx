0d380b08581413850f45452b5c4aac81811b9823
Sourabh Subhash Ingle <Sourabh.ingle15@gmail.com>
2025-07-17 17:27:14 +0530

    add new currency convertor

--- 01passwordgenerator/src/App.jsx
+++ 01passwordgenerator/src/App.jsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+import CurrencyConvertor from './components/currencyConvertor/CurrencyConvertor'
 import PasswordConverter from './components/passwordGen/PasswordConverter'
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
 
   return (
     <>
+      < CurrencyConvertor />
       <PasswordConverter />
     </>
   )
---
+++ 01passwordgenerator/src/components/currencyConvertor/CurrencyConvertor.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const CurrencyConvertor = () => {
+    return (
+        <div className='container'>
+            <h2 className='text-3xl font-bold text-center'>Currency Convertor</h2>
+            <div className="card">
+                
+            </div>
+
+            
+       </div>
+  )
+}
+
+export default CurrencyConvertor;
