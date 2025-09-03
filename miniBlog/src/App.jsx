f1a51157ecac86ce20a598698d19f92ef6c0a6a2
Sourabh Subhash Ingle <Sourabh.ingle15@gmail.com>
2025-09-04 02:31:34 +0530

    add redux and set all

--- miniBlog/src/App.jsx
+++ miniBlog/src/App.jsx
@@ -1,16 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+import { useDispatch } from 'react-redux';
+import { useState,useEffect } from 'react';
+import './App.css';
+import authService from './appwrite/auth';
+import { login, logout } from "./stores/authSlice";
+import { Header,Footer } from './components';
+import { Outlet } from 'react-router-dom';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
 
-  return (
-    <>
-     <h1>Hello Everyone</h1>
-    </>
-  )
+  useEffect(() => {
+    authService.getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({userData}))
+        } else {
+          dispatch(logout())
+        }
+      })
+      .catch((error) => {
+        throw error
+      })
+      .finally(()=>setLoading(false))
+  }, [])
+  
+
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+          <main>
+            <Outlet/>
+          </main>
+        <Footer />
+      </div>
+    </div>
+  ) : (null)
 }
 
 export default App
---
+++ miniBlog/src/stores/authSlice.js
@@ -0,0 +1,26 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    status: false,
+    userData:null
+}
+
+const authSlice = createSlice({
+    name: "auth",
+    initialState,
+    reducer: {
+        login: (state, action) => {
+            state.status = true,
+            state.userData=action.payload.userData
+        },
+        logout: (state) => {
+            state.status = false,
+            state.userDate=null
+        },
+    }
+})
+
+export const { login,logout}=authSlice.actions;  
+
+export default authSlice.reducer;   
