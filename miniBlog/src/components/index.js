9678d9a97e4ef15a1e6ce467fdae91787e236b04
Sourabh Subhash Ingle <Sourabh.ingle15@gmail.com>
2025-09-04 23:33:51 +0530

    Select component add

--- miniBlog/src/components/index.js
+++ miniBlog/src/components/index.js
@@ -1,13 +1,17 @@
-import Header from './header/Header';
+import Header from './header/Header.jsx';
 import Footer from './footer/Footer';
 import Container from './container/Container';
 import Logo from './Logo';
 import LogoutBtn from './header/LogoutBtn';
+import Select from './select/Select';
+import Input from './input/Input';
+import Button from './Button'
+
 
 
 
 
 export {
     Header, Footer,
-    Container,LogoutBtn,Logo
+    Container,LogoutBtn,Logo,Input,Select,Button
 } 
