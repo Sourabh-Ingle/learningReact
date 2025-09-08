1dd55612cfd7020c276c6be2df5cd3bdeaba97e1
Sourabh Subhash Ingle <Sourabh.ingle15@gmail.com>
2025-09-09 01:58:50 +0530

    postForm component

--- miniBlog/src/components/index.js
+++ miniBlog/src/components/index.js
@@ -7,12 +7,16 @@ import Select from './select/Select';
 import Input from './input/Input';
 import Button from './Button';
 import Select from './select/Select';
-
+import RTE from './rte/RTE.jsx';
+import Signup from './signup/Signup.jsx';
+import PostCard from './postCard/PostCard.jsx';
+import PostForm from './post-form/PostForm.jsx'
+import Login from './login/Login.jsx';
 
 
 
 
 export {
-    Header, Footer,
-    Container,LogoutBtn,Logo,Input,Select,Button,Select
+    Header, Footer, Container, LogoutBtn, Logo, Input,
+    Select, Button, Select, RTE, Signup, PostCard, PostForm,Login
 } 
